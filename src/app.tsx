d987b5a6fcb4306ec1f4f227b9bc7bbea7ab903d
Pablo Alan <pabloxt14@gmail.com>
2024-07-12 21:41:03 -0300

    feat: config tailwindcss

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,10 @@
 export function App() {
   return (
-    <div>
-      <h1 className="bg-red-500 text-3xl font-bold">Hello World</h1>
+    <div className="p-2">
+      <h1 className="text-2xl text-zinc-900">Hello World</h1>
+      <button className="rounded bg-violet-500 px-3 py-1.5 font-bold text-white">
+        Click me
+      </button>
     </div>
   )
 }
