7e13273f34567bf39d91592a34adf3150b8fd4c4
Pablo Alan <pabloxt14@gmail.com>
2024-07-13 20:00:00 -0300

    feat: create guests input logic

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,25 @@
-import { LuArrowRight, LuCalendar, LuMapPin } from 'react-icons/lu'
+import { useState } from 'react'
+import {
+  LuArrowRight,
+  LuCalendar,
+  LuMapPin,
+  LuSettings2,
+  LuUserPlus2,
+} from 'react-icons/lu'
 
 export function App() {
+  const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false)
+
+  function openGuestsInput() {
+    setIsGuestsInputOpen(true)
+  }
+
+  function closeGuestsInput() {
+    setIsGuestsInputOpen(false)
+  }
+
   return (
-    <div className="bg-squares flex min-h-screen items-center justify-center bg-center bg-no-repeat">
+    <div className="flex min-h-screen items-center justify-center bg-squares bg-center bg-no-repeat">
       <div className="w-full max-w-3xl space-y-10 px-6 text-center">
         <div className="flex flex-col items-center gap-3">
           <img src="/logo.svg" alt="plann.er" />
@@ -11,31 +28,68 @@ export function App() {
           </p>
         </div>
 
-        <div className="shadow-shape flex h-16 items-center gap-3 rounded-xl bg-zinc-900 px-4">
-          <div className="flex flex-1 items-center gap-2">
-            <LuMapPin className="size-5 text-zinc-400" />
-            <input
-              type="text"
-              placeholder="Para onde você vai?"
-              className="flex-1 bg-transparent text-lg placeholder-zinc-400"
-            />
+        <div className="space-y-4">
+          <div className="flex h-16 items-center gap-3 rounded-xl bg-zinc-900 px-4 shadow-shape">
+            <div className="flex flex-1 items-center gap-2">
+              <LuMapPin className="size-5 text-zinc-400" />
+              <input
+                disabled={isGuestsInputOpen}
+                type="text"
+                placeholder="Para onde você vai?"
+                className="flex-1 bg-transparent text-lg placeholder-zinc-400"
+              />
+            </div>
+
+            <div className="flex items-center gap-2">
+              <LuCalendar className="size-5 text-zinc-400" />
+              <input
+                disabled={isGuestsInputOpen}
+                type="text"
+                placeholder="Quando?"
+                className="w-full max-w-20 bg-transparent text-lg placeholder-zinc-400"
+              />
+            </div>
+
+            <div className="h-6 w-px bg-zinc-800" />
+
+            {isGuestsInputOpen ? (
+              <button
+                onClick={closeGuestsInput}
+                className="flex items-center gap-2 rounded-lg bg-zinc-800 px-5 py-2 font-medium text-zinc-200 transition-colors hover:bg-zinc-700"
+              >
+                Alterar local/data
+                <LuSettings2 className="size-5" />
+              </button>
+            ) : (
+              <button
+                onClick={openGuestsInput}
+                className="flex items-center gap-2 rounded-lg bg-lime-300 px-5 py-2 font-medium text-lime-950 transition-colors hover:bg-lime-400"
+              >
+                Continuar
+                <LuArrowRight className="size-5" />
+              </button>
+            )}
           </div>
 
-          <div className="flex items-center gap-2">
-            <LuCalendar className="size-5 text-zinc-400" />
-            <input
-              type="text"
-              placeholder="Quando?"
-              className="w-full max-w-20 bg-transparent text-lg placeholder-zinc-400"
-            />
-          </div>
+          {isGuestsInputOpen && (
+            <div className="flex h-16 items-center gap-3 rounded-xl bg-zinc-900 px-4 shadow-shape">
+              <div className="flex flex-1 items-center gap-2">
+                <LuUserPlus2 className="size-5 text-zinc-400" />
+                <input
+                  type="text"
+                  placeholder="Quem estarão na viagem?"
+                  className="flex-1 bg-transparent text-lg placeholder-zinc-400"
+                />
+              </div>
 
-          <div className="h-6 w-px bg-zinc-800" />
+              <div className="h-6 w-px bg-zinc-800" />
 
-          <button className="flex items-center gap-2 rounded-lg bg-lime-300 px-5 py-2 font-medium text-lime-950 transition-colors hover:bg-lime-400">
-            Continuar
-            <LuArrowRight className="size-5" />
-          </button>
+              <button className="flex items-center gap-2 rounded-lg bg-lime-300 px-5 py-2 font-medium text-lime-950 transition-colors hover:bg-lime-400">
+                Confirmar viagem
+                <LuArrowRight className="size-5" />
+              </button>
+            </div>
+          )}
         </div>
 
         <p className="mx-auto w-full max-w-[30rem] text-sm text-zinc-500">
