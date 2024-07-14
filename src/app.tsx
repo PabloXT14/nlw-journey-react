9a2d091f0b7630b82defd5714bd5b71a5018649c
Pablo Alan <pabloxt14@gmail.com>
2024-07-14 01:11:39 -0300

    feat: create gusts modal

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import {
   LuArrowRight,
+  LuAtSign,
   LuCalendar,
   LuMapPin,
+  LuPlus,
   LuSettings2,
   LuUserPlus2,
+  LuX,
 } from 'react-icons/lu'
 
 export function App() {
   const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false)
+  const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false)
 
   function openGuestsInput() {
     setIsGuestsInputOpen(true)
@@ -16,6 +20,14 @@ export function App() {
 
   function closeGuestsInput() {
     setIsGuestsInputOpen(false)
+  }
+
+  function openGuestsModal() {
+    setIsGuestsModalOpen(true)
+  }
+
+  function closeGuestsModal() {
+    setIsGuestsModalOpen(false)
   }
 
   return (
@@ -73,14 +85,16 @@ export function App() {
 
           {isGuestsInputOpen && (
             <div className="flex h-16 items-center gap-3 rounded-xl bg-zinc-900 px-4 shadow-shape">
-              <div className="flex flex-1 items-center gap-2">
+              <button
+                type="button"
+                onClick={openGuestsModal}
+                className="flex flex-1 items-center gap-2 text-left"
+              >
                 <LuUserPlus2 className="size-5 text-zinc-400" />
-                <input
-                  type="text"
-                  placeholder="Quem estarão na viagem?"
-                  className="flex-1 bg-transparent text-lg placeholder-zinc-400"
-                />
-              </div>
+                <span className="flex-1 text-lg text-zinc-400">
+                  Quem estarão na viagem?
+                </span>
+              </button>
 
               <div className="h-6 w-px bg-zinc-800" />
 
@@ -105,6 +119,63 @@ export function App() {
           .
         </p>
       </div>
+      {isGuestsModalOpen && (
+        <div className="items fixed inset-0 flex items-center justify-center bg-black/60">
+          <div className="w-full max-w-[640px] space-y-5 rounded-xl bg-zinc-900 px-6 py-5 shadow-shape">
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold">Selecionar convidados</h2>
+                <button onClick={closeGuestsModal} className="text-zinc-400">
+                  <LuX className="size-5" />
+                </button>
+              </div>
+
+              <p className="text-sm text-zinc-400">
+                Os convidados irão receber e-mails para confirmar a participação
+                na viagem.
+              </p>
+            </div>
+
+            <div className="flex flex-wrap gap-2">
+              <div className="flex items-center gap-2 rounded-md bg-zinc-800 px-2.5 py-1.5">
+                <span className="text-zinc-300">jhondoe@example.com</span>
+                <button type="button" className="text-zinc-400">
+                  <LuX className="size-4" />
+                </button>
+              </div>
+              <div className="flex items-center gap-2 rounded-md bg-zinc-800 px-2.5 py-1.5">
+                <span className="text-zinc-300">jhondoe@example.com</span>
+                <button type="button" className="text-zinc-400">
+                  <LuX className="size-4" />
+                </button>
+              </div>
+            </div>
+
+            <div className="h-px w-full bg-zinc-800" />
+
+            <form className="flex items-center gap-2 rounded-lg border border-zinc-800 bg-zinc-950 p-2.5">
+              <div className="flex flex-1 items-center gap-2 px-2">
+                <LuAtSign className="size-5 text-zinc-400" />
+
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Digite o email do convidado"
+                  className="flex-1 bg-transparent text-lg placeholder-zinc-400"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="flex items-center gap-2 rounded-lg bg-lime-300 px-5 py-2 font-medium text-lime-950 transition-colors hover:bg-lime-400"
+              >
+                Convidar
+                <LuPlus className="size-5" />
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
