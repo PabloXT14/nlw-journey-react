6fe4326d5acadf8cf4c6a8c6dce7074221e13538
Pablo Alan <pabloxt14@gmail.com>
2024-07-14 16:09:08 -0300

    feat: create confirm-trip-modal

--- src/app.tsx
+++ src/app.tsx
@@ -3,9 +3,11 @@ import {
   LuArrowRight,
   LuAtSign,
   LuCalendar,
+  LuMail,
   LuMapPin,
   LuPlus,
   LuSettings2,
+  LuUser,
   LuUserPlus2,
   LuX,
 } from 'react-icons/lu'
@@ -17,6 +19,8 @@ export function App() {
     'johndoe@example.com',
     'marydoe@example.com',
   ])
+
+  const [isConfirmTripModalOpen, setIsConfirmTripModalOpen] = useState(false)
 
   function openGuestsInput() {
     setIsGuestsInputOpen(true)
@@ -32,6 +36,14 @@ export function App() {
 
   function closeGuestsModal() {
     setIsGuestsModalOpen(false)
+  }
+
+  function openConfirmTripModal() {
+    setIsConfirmTripModalOpen(true)
+  }
+
+  function closeConfirmTripModal() {
+    setIsConfirmTripModalOpen(false)
   }
 
   function addNewEmailToInvite(event: FormEvent<HTMLFormElement>) {
@@ -129,7 +141,10 @@ export function App() {
 
               <div className="h-6 w-px bg-zinc-800" />
 
-              <button className="flex items-center gap-2 rounded-lg bg-lime-300 px-5 py-2 font-medium text-lime-950 transition-colors hover:bg-lime-400">
+              <button
+                onClick={openConfirmTripModal}
+                className="flex items-center gap-2 rounded-lg bg-lime-300 px-5 py-2 font-medium text-lime-950 transition-colors hover:bg-lime-400"
+              >
                 Confirmar viagem
                 <LuArrowRight className="size-5" />
               </button>
@@ -208,6 +223,68 @@ export function App() {
               >
                 Convidar
                 <LuPlus className="size-5" />
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+
+      {isConfirmTripModalOpen && (
+        <div className="items fixed inset-0 flex items-center justify-center bg-black/60">
+          <div className="w-full max-w-[640px] space-y-5 rounded-xl bg-zinc-900 px-6 py-5 shadow-shape">
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold">
+                  Confirmar criação de viagem
+                </h2>
+                <button
+                  onClick={closeConfirmTripModal}
+                  className="text-zinc-400"
+                >
+                  <LuX className="size-5" />
+                </button>
+              </div>
+
+              <p className="text-sm text-zinc-400">
+                Para concluir a criação da viagem para{' '}
+                <span className="font-semibold text-zinc-100">
+                  Florianópolis, Brasil
+                </span>{' '}
+                nas datas de{' '}
+                <span className="font-semibold text-zinc-100">
+                  16 a 27 de Agosto de 2024
+                </span>{' '}
+                preencha seus dados abaixo:
+              </p>
+            </div>
+
+            <form onSubmit={addNewEmailToInvite} className="space-y-3">
+              <div className="flex h-14 flex-1 items-center gap-2 rounded-lg border border-zinc-800 bg-zinc-950 px-5">
+                <LuUser className="size-5 text-zinc-400" />
+
+                <input
+                  name="name"
+                  placeholder="Seu nome completo"
+                  className="flex-1 bg-transparent text-lg placeholder-zinc-400"
+                />
+              </div>
+
+              <div className="flex h-14 flex-1 items-center gap-2 rounded-lg border border-zinc-800 bg-zinc-950 px-5">
+                <LuMail className="size-5 text-zinc-400" />
+
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Seu email pessoal"
+                  className="flex-1 bg-transparent text-lg placeholder-zinc-400"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="flex h-11 w-full items-center justify-center gap-2 rounded-lg bg-lime-300 px-5 font-medium text-lime-950 transition-colors hover:bg-lime-400"
+              >
+                Confirmar criação da viagem
               </button>
             </form>
           </div>
