1d6d2b5e0010368b77e413bff0ebaff80776ee68
Pablo Alan <pabloxt14@gmail.com>
2024-07-16 16:51:25 -0300

    feat: show participants details on guests

--- src/pages/trip-details/guests.tsx
+++ src/pages/trip-details/guests.tsx
@@ -1,26 +1,55 @@
-import { LuCircleDashed, LuUserCog } from 'react-icons/lu'
+import { LuCheckCircle2, LuCircleDashed, LuUserCog } from 'react-icons/lu'
 import { Button } from '../../components/button'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { api } from '../../lib/axios'
+
+type Participant = {
+  id: string
+  name: string | null
+  email: string
+  is_confirmed: boolean
+}
 
 export const Guests = () => {
+  const { tripId } = useParams()
+
+  const [participants, setParticipant] = useState<Participant[]>([])
+
+  useEffect(() => {
+    api.get(`/trips/${tripId}/participants`).then((response) => {
+      setParticipant(response.data.participants)
+    })
+  }, [tripId])
+
   return (
     <div className="space-y-6">
       <h2 className="text-xl font-semibold">Convidados</h2>
 
       {/* Guest list */}
       <div className="space-y-5">
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="block font-medium text-zinc-100">
-              Jessica White
-            </span>
-            <span className="block truncate text-sm text-zinc-400">
-              jessice.white@gmail.com
-            </span>
+        {participants?.map((participant, index) => (
+          <div
+            key={participant.id}
+            className="flex items-center justify-between gap-4"
+          >
+            <div className="space-y-1.5">
+              <span className="block font-medium text-zinc-100">
+                {participant.name ?? `Convidado ${index}`}
+              </span>
+              <span className="block truncate text-sm text-zinc-400">
+                {participant.email}
+              </span>
+            </div>
+            {participant.is_confirmed ? (
+              <LuCheckCircle2 className="size-5 shrink-0 text-lime-300" />
+            ) : (
+              <LuCircleDashed className="size-5 shrink-0 text-zinc-400" />
+            )}
           </div>
-          <LuCircleDashed className="size-5 shrink-0 text-zinc-400" />
-        </div>
+        ))}
 
-        <div className="flex items-center justify-between gap-4">
+        {/* <div className="flex items-center justify-between gap-4">
           <div className="space-y-1.5">
             <span className="block font-medium text-zinc-100">
               Dr. Rita Pacocha
@@ -30,7 +59,7 @@ export const Guests = () => {
             </span>
           </div>
           <LuCircleDashed className="size-5 shrink-0 text-zinc-400" />
-        </div>
+        </div> */}
       </div>
 
       <Button variant="secondary" size="full">
